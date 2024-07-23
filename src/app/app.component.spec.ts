e75371a5a7154960952d59ed531390e0012988f9
AminEl93 <amin_11_iverson@hotmail.com>
2024-07-23 22:24:36 +0200

    Navbar y Footer iniciales

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,18 +12,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'CentreIslamicVicApp' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('CentreIslamicVicApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CentreIslamicVicApp');
-  });
+  });    
 });
